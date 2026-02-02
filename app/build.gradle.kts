4c72ec4203e2f52cdaa098c63004feafd1bd173a
Akash Sarkar <akash.sarkar188@gmail.com>
2026-02-02 22:40:09 +0530

    [VERSION-BUMP] -- 1.0.0(1) -> 1.0.1(2)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.akashsarkar188.gitrelease"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         manifestPlaceholders["appAuthRedirectScheme"] = "github.akashsarkar188.debughelper"
